13ee586b4b79d9540eac87da2bf706405790a316
coolman0123 <socool4m@naver.com>
2025-01-17 18:54:54 +0900

    feat: 초기 빈 파일일 때 미리 빈 list 생성

--- src/model/dao/UserDao.java
+++ src/model/dao/UserDao.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import model.User;
 
-//파일에서 list<User> 정보를 읽어오거나 저장하는 기능
 public class UserDao {
     private String filename;
 
@@ -17,27 +16,31 @@ public class UserDao {
         this.filename = filename;
     }
 
-    //리스트를 받아와 저장
+    // 리스트를 받아와 저장
     public void saveUser(List<User> list) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
             out.writeObject(list);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
     }
 
-    //파일로부터 읽어들여 저장
-    public List<User> loadUser() {
+    // 파일로부터 읽어들여 저장
+    public List<User> loadUsers() {
         File file = new File(filename);
 
-        if (!file.exists()) {
+        // 파일이 존재하지 않거나 비어 있으면 빈 리스트 반환
+        if (!file.exists() || file.length() == 0) {
             return new ArrayList<>();
         }
 
-        List<User> list = null;
+        List<User> list = new ArrayList<>();
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
+            // 파일에 객체가 있으면 읽어오기
             list = (List<User>) in.readObject();
+        } catch (java.io.EOFException ex) {
+            // EOFException 발생 시 빈 리스트 반환
+            return new ArrayList<>();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
