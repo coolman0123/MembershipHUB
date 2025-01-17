3e1b5581981d6459b4c217c7ea87d3c5fb64937f
coolman0123 <socool4m@naver.com>
2025-01-17 20:04:06 +0900

    커밋 로그 확인

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         UserMembershipController userMembershipController = new UserMembershipController();
         userMembershipController.run();
+        System.out.println("sss");
     }
 
 }
