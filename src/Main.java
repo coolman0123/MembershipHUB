efc4bd99783c72cce8640b1b2c242689f83bbc6a
coolman0123 <socool4m@naver.com>
2025-01-17 18:57:53 +0900

    feat: controller 생성 전체 프로그램 시작점 구현

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import controller.UserMembershipController;
+
 public class Main {
+    public static void main(String[] args) {
+        UserMembershipController userMembershipController = new UserMembershipController();
+        userMembershipController.run();
+    }
 
 }
